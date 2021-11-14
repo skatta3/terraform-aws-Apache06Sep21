3dd8a8176f75fc3f266b988b4ce397dea9f48684
skatta3 <skatta3@yahoo.com>
2021-11-14 07:32:41 +0530

    Terraform Modules

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.accesskey}"
+  secret_key = "${var.secretkey}"
+}
+
+resource "aws_instance" "myfirst" {
+  ami             = "${var.imageid}"
+  instance_type   = "${var.instancetype}"
+  key_name        = "${var.key}"
+  security_groups = ["${aws_security_group.allow_all.name}"]
+
+provisioner "remote-exec" {
+    inline                  = [
+        "sudo yum install httpd -y",
+	"sudo service httpd start"
+    ]
+
+    connection {
+        type                = "ssh"
+        user                = "ec2-user"
+        private_key         = file("./${var.key1}")
+        host                = aws_instance.myfirst.public_ip
+    }
+  }
+}
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${var.vpcID}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+ default = "" 
+}
+
+variable "secretkey" {
+  default = ""
+}
+
+variable "imageid" {
+  default = "ami-0b0af3577fe5e3532"
+}
+
+variable "key" {
+  default = "AnjaliEC2Keys"
+}
+
+variable "instancetype" {
+  default = "t2.micro"
+}
+
+variable "vpcID" {
+	default="vpc-4ecd1533"
+}
+
+variable "key1" {
+  default = "AnjaliEC2Keys.pem"
+}
+
+
